549b137a3b43c531b5fa57af578b6d12e78f4f69
Stewart Renehan <stewart@pellego.com>
2017-02-26 16:04:51 -0800

    Updated data cleaning

--- scripts/lm_script.R
+++ scripts/lm_script.R
@@ -1,18 +1,19 @@
+library(MASS)
 library(ggmap)
 library(ggplot2)
 library(dplyr)
 library(faraway)
 
 # setwd('D:\\Program File\\Git\\git_projects\\STATS 504\\ProjectProposal\\stat-504-airbnb\\data')
-#setwd('/Users/stewart/projects/stat-504-airbnb/data/');
-setwd("~/Documents/courses/504/stat-504-airbnb/data")
+setwd('/Users/stewart/projects/stat-504-airbnb/data/');
+# setwd("~/Documents/courses/504/stat-504-airbnb/data")
 
 df.data <- read.csv('listings.csv',header=TRUE,sep=',',stringsAsFactors=FALSE)
 
 #Reducing to variables of interests
 df.data <- dplyr::select(df.data, price, host_is_superhost, host_has_profile_pic, host_identity_verified, 
-                  neighbourhood_cleansed, property_type, room_type, accommodates, bathrooms, bedrooms, beds, bed_type,
-                  amenities, guests_included, minimum_nights)
+                         neighbourhood_cleansed, property_type, room_type, accommodates, bathrooms, bedrooms, beds, bed_type,
+                         amenities, guests_included, minimum_nights, number_of_reviews)
 
 ## cleaning data
 # Coerce columns into proper types
@@ -69,7 +70,14 @@ df.data <- dplyr::select(df.data, -amenities)
 for (var in names(df.data)) {
   df.data = df.data[!is.na(df.data[,var]),]
 }
-nrow(df.data)  # 9574
+nrow(df.data)  # 13767
+
+sum(df.data$number_of_reviews == 0)  # 2451
+# Listings with no reviews have few stays; their prices may not be indicitive of what they are worth.
+df.data = df.data[!df.data$number_of_reviews == 0,]
+nrow(df.data)  # 11316
+#Drop number of reviews
+df.data <- dplyr::select(df.data, -number_of_reviews)
 
 summary(df.data$property_type)
 # These types have less than 5 counts
@@ -77,50 +85,37 @@ property_types_to_remove = c(
   "Bungalow", "Chalet", "Earth House", "Dorm", "Hut", "Tent", "Yurt"
 )
 df.data = df.data[!df.data$property_type %in% property_types_to_remove,]
-nrow(df.data)  # 9562
+nrow(df.data)  # 11304
+
+# Remove listings with accommodates outliers. 
+df.data <- subset(df.data, !(accommodates > 10 & price < 150))
+nrow(df.data)  # 11295
 
 
 # Build model
-lm.airbnb <- lm(price~., data=df.data[,-1])
+lm.airbnb <- lm(price~., data=df.data)
 summary(lm.airbnb)
 plot(lm.airbnb$fitted.values, lm.airbnb$residuals) # Clear violation of constant variance
 
 # Remove outliers
-model.influence = influence(lm.airbnb)
-halfnorm(model.influence$hat,
-         xlim=c(-0.1,4.8),
-         ylab="Leverages")
-title(main="Half-Normalquantileplotforleverages")
-max(model.influence$hat) # 0.0997
-# No obvious leverage points
-
 # Cooks distances
 cook = cooks.distance(lm.airbnb)
-halfnorm(cook) # 422, 221
-# 422 is removed because it only has 0.11 removed per month and costs $1307 per night.
-# 221 is removed because it only has 1 review and it accomodates 16 but only costs $100.
-bad_ids = c(11946090, 5587500)
-df.data = df.data[!df.data$id %in% bad_ids,]
-lm.airbnb <- lm(price~., data=df.data[,-1])
-cook = cooks.distance(lm.airbnb)
-halfnorm(cook) # 276, 218
-# 276 accommodates 16 but only costs $60, something is wrong
-# 218 sems normal
-df.data = df.data[-276,]
-lm.airbnb <- lm(price~., data=df.data[,-1])
-cook = cooks.distance(lm.airbnb)
-halfnorm(cook) # 317, 868
-# 317 accommodates 16 and only costs 50, something is wrong
-# This is recurring, something is strange
-sum(df.data$accommodates == 16) # 21
-# These are outliers with wrong data.
-df.data = df.data[!df.data$accommodates == 16,]
-nrow(df.data)  # 9538
+halfnorm(cook) # 487, 5703
+# 487 is removed because it only has 0.11 removed per month and costs $1307 per night.
+# 5703 is removed because it has a minimum_nights value of 1000
+bad_ids = c(487, 5703)
+df.data = df.data[-bad_ids,]
 
-lm.airbnb <- lm(price~., data=df.data[,-1])
+# Remove points with minimum_nights > 15. These dont fall under the category of short term vacation
+# rental. 
+df.data = df.data[df.data$minimum_nights <= 15,]
+nrow(df.data)  # 11223
+
+lm.airbnb <- lm(price~., data=df.data)
 cook = cooks.distance(lm.airbnb)
-halfnorm(cook) # 354, 509
-# These two are both > $400, but seem normal. I am stopping here.
+halfnorm(cook) # 100, 595
+# 100 seems normal, but cheap.
+# 595 sems normal but more expensive. Stop here
 
 plot(lm.airbnb, which=1)
 plot(lm.airbnb$fitted.values,
@@ -130,16 +125,21 @@ plot(lm.airbnb$fitted.values,
      main="Residuals vs Fitted Values") # Clear violation of constant variance
 
 ##transformation 
-library('MASS')
 boxcox(lm.airbnb)
 ##we can choose lambda =0 and transform price to log(price)
-lm.airbnb=lm(log(price)~.,data=df.data[,-1])
+lm.airbnb=lm(log(price)~.,data=df.data)
 summary(lm.airbnb)
 plot(lm.airbnb,which=1) 
 
 ##variable selection
 step(lm.airbnb)
 ##from the output of step, we exclude beds, host_acceptance_rate,review_scores_checkin, review_scores_communication.
+
+# exhaustive variable selection
+library(leaps)
+rs<-summary(regsubsets(price~.,data=df.data, really.big = T))
+plot(2:10,rs$cp,xlab="Numberofparameters",ylab="MallowsC_p") # Monotonically decreasing;
+# Dont throw out any variables (and look for more to add)
 
 drop=c('beds','host_acceptance_rate','review_scores_checkin','review_scores_communication')
 df.data.drop=df.data[,!names(df.data)%in%drop]
@@ -178,10 +178,3 @@ feature_names = c(
 barplot(features.importances, names.arg = feature_names,
         ylab = "Improvement to Adj R-Squared",
         main = "Feature Importance",las=3)
-
-
-
-
-
-
-
